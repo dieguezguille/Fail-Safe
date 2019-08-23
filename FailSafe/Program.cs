4d48005e0421e0cd2ae75ba7a99b569e1d3ad66e
Guillermo Diéguez <guillermodieguez@outlook.com>
2019-08-23 15:56:30 -0300

    Primer Commit

---
+++ FailSafe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace FailSafe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // make sure to locate program in the solution folder
+            // var path = "C:\\Users\\Guille\\Desktop\\Archivos\\Silentium-Repos\\Aliviar-API\\";
+            var path = Directory.GetCurrentDirectory();
+
+            RunTasks(path);
+        }
+
+        public static void RunTasks(string path)
+        {
+            TaskRunner.CloseVsStudio();
+            TaskRunner.DeleteBinAndObj(path);
+            TaskRunner.DeleteVsFolder(path);
+        }
+    }
+}
---
+++ FailSafe/TaskRunner.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FailSafe
+{
+    public class TaskRunner
+    {
+        public static void CloseVsStudio()
+        {
+            var processes = System.Diagnostics.Process.GetProcesses().Where(p => p.ProcessName.Contains("devenv"));
+
+            foreach (System.Diagnostics.Process process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        public static void DeleteBinAndObj(string path)
+        {
+            try
+            {
+                // directories to search
+                var bin = Directory.EnumerateFileSystemEntries(path, "bin", SearchOption.AllDirectories);
+                var obj = Directory.EnumerateFileSystemEntries(path, "obj", SearchOption.AllDirectories);
+
+                var dirs = bin.Concat(obj);
+
+                if (dirs != null && dirs.Count() > 0)
+                {
+                    Console.WriteLine("Se encontraron los siguientes directorios:");
+                    Console.WriteLine("");
+
+                    foreach (var dir in dirs)
+                    {
+                        Console.WriteLine(dir);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron directorios para eliminar. Cierre Visual Studio e intente nuevamente.");
+                    Console.ReadLine();
+                    return;
+                }
+
+                // delete directories
+                Console.WriteLine("");
+                Console.WriteLine("Presione ENTER para eliminar o CTRL + C para cancelar.");
+                Console.WriteLine("");
+                Console.ReadLine();
+
+                Console.WriteLine("Borrando directorios...");
+                Console.WriteLine("");
+
+                foreach (var dir in dirs)
+                {
+                    // delete all
+                    Directory.Delete(dir, true);
+                }
+
+                Console.WriteLine("Los directorios se eliminaron exitosamente. Presione ENTER para continuar.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                // handle exception
+                Console.WriteLine("La operación falló con el siguiente error:");
+                Console.WriteLine("");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("");
+            }
+        }
+
+        public static void DeleteVsFolder(string path)
+        {
+            try
+            {
+                // get .vs folder
+                string[] dirs = Directory.GetDirectories(path);
+                var vsFolder = dirs
+                    .Where(dir => dir.Contains("vs"))
+                    .FirstOrDefault();
+
+                if (vsFolder != null)
+                {
+                    Console.WriteLine("Se encontró la varpeta '.vs' en la siguiente ubicación:");
+                    Console.WriteLine("");
+                    Console.WriteLine(vsFolder);
+                    Console.WriteLine("");
+                    Console.WriteLine("Presione ENTER para eliminar o CTRL + C para cancelar.");
+                    Console.ReadLine();
+
+                    // delete all
+                    Directory.Delete(vsFolder, true);
+
+                    Console.WriteLine("El directorio se eliminó exitosamente. Presione ENTER para SALIR.");
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró la carpeta '.vs'. Asegúrese de que existe e intente nuevamente.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                // handle exception
+                Console.WriteLine("La operación falló con el siguiente error:");
+                Console.WriteLine("");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Presione ENTER para SALIR.");
+                Console.WriteLine("");
+                Console.ReadLine();
+            }
+        }
+        //private static void GetConfigFile(string path)
+        //{
+        //    // get applicationhost.config file
+        //    var file = Directory
+        //        .GetFiles(path, "applicationhost.config", SearchOption.AllDirectories)
+        //        .FirstOrDefault();
+
+        //    if (file != null && file.Count() > 0)
+        //    {
+        //        Console.WriteLine("Se encontró el archivo .config en la siguiente ubicación:");
+        //        Console.WriteLine("");
+        //        Console.WriteLine(file);
+        //        Console.WriteLine("");
+        //        Console.WriteLine("Presione ENTER para modificar o CTRL + C para cancelar");
+        //        Console.ReadLine();
+        //        EditConfigFile(file);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No se encontraron archivos para editar. Cierre Visual Studio e intente nuevamente.");
+        //        Console.ReadLine();
+        //        return;
+        //    }
+        //}
+
+        //private static void EditConfigFile(string file)
+        //{
+        //    // get local ip address
+        //    var ip = GetLocalIp();
+
+        //    if (ip.Length < 16) //IPv4 received
+        //    {
+        //        Console.WriteLine("La dirección IP local es:");
+        //        Console.WriteLine("");
+        //        Console.WriteLine(ip);
+        //        Console.WriteLine("");
+        //        Console.WriteLine("Presione ENTER para actualizar el archivo .config o CTRL + C para cancelar");
+        //        Console.ReadLine();
+
+
+        //    }
+        //    else
+        //    {
+        //        //handle error
+        //        Console.WriteLine("");
+        //    }
+
+        //    // edit .config file
+        //}
+
+        //private static string GetLocalIp()
+        //{
+        //    var host = Dns.GetHostEntry(Dns.GetHostName());
+
+        //    foreach (var ip in host.AddressList)
+        //    {
+        //        if (ip.AddressFamily == AddressFamily.InterNetwork)
+        //        {
+        //            return ip.ToString();
+        //        }
+        //    }
+
+        //    return("No se encontró una dirección IPv4. Asegúrese de estar conectado a una red.");
+        //}
+    }
+}
