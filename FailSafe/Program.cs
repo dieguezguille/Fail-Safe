8f89435d9cede4894d8b5cc34a278deb70838117
Guillermo Diéguez <guillermodieguez@outlook.com>
2019-09-11 11:33:33 -0300

    Updated scrfipt

--- FailSafe/Program.cs
+++ FailSafe/Program.cs
@@ -2,22 +2,23 @@
 
 namespace FailSafe
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // make sure to locate program in the solution folder
-            // var path = "C:\\Users\\Guille\\Desktop\\Archivos\\Silentium-Repos\\Aliviar-API\\";
-            var path = Directory.GetCurrentDirectory();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// make sure to locate program in the solution folder
+			// var path = "C:\\Users\\Guille\\Desktop\\Archivos\\Silentium-Repos\\Aliviar-API\\";
+			var path = Directory.GetCurrentDirectory();
 
-            RunTasks(path);
-        }
+			RunTasks(path);
+		}
 
-        public static void RunTasks(string path)
-        {
-            TaskRunner.CloseVsStudio();
-            TaskRunner.DeleteBinAndObj(path);
-            TaskRunner.DeleteVsFolder(path);
-        }
-    }
+		public static void RunTasks(string path)
+		{
+			TaskRunner.CloseVsStudio();
+			TaskRunner.DeleteBinAndObj(path);
+			TaskRunner.DeleteVsFolder(path);
+			TaskRunner.ReopenVsStudio(2);
+		}
+	}
 }
